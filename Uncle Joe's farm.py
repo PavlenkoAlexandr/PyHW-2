0de708d5bb84f35574008053c5796d1e05a33520
Alexandr Pavlenko <alcohollex2@gmail.com>
2020-09-29 22:09:01 +0300

    Uncle Joe's farm.py edit

--- Uncle Joe's farm.py
+++ Uncle Joe's farm.py
@@ -1,3 +1,9 @@
+class AnimalDB():
+    animals_list = list()
+
+    def add_animal(self, animal):
+        self.animals_list.append(animal)
+
 class Animal:
     kind = ''
     name = ''
@@ -5,141 +11,89 @@ class Animal:
     product = ''
     noise = ''
 
-    def feed(self, food):
-        self.weight += food
+    def feed(self):
+        print(f'{self.kind} {self.name} покормлен! "Ом-ном-ном"')
 
     def make_noise(self):
         print(f'{self.kind} {self.name} говорит: {self.noise}!')
 
-    def harvest(self, number, quantity):
-        print(f'Ресурс "{self.product.lower()}" в количестве {number} {quantity} собран.')
 
-class Goose(Animal):
+class Egg_Givers(Animal):
+    def harvest_eggs(self):
+        print(f'Яйца собраны')
+
+class Goose(Egg_Givers):
     kind = 'Гусь'
-    product = 'Перо'
     noise = 'Га-га-га'
 
-class AnimalDB():
-    animals_list = list()
-
-    def add_animal(self, animal):
-        self.animals_list.append(animal)
-
-class Cow(Animal):
-    kind = 'Корова'
-    product = 'Молоко'
-    noise = 'Му-у-у'
-
-class Chiken(Animal):
+class Chiken(Egg_Givers):
     kind = 'Кура'
-    product = 'Яйцо'
     noise = 'Ко-ко-ко'
 
-class Duck(Animal):
+class Duck(Egg_Givers):
     kind = 'Утка'
-    product = 'Перо'
     noise = 'Кря-кря-кря'
 
-class Sheep(Animal):
-    kind = 'Овца'
-    product = 'Шерсть'
-    noise = 'Бе-е-е'
+class Milk_Givers(Animal):
+    def harvest_milk(self):
+        print(f'Молоко собрано')
 
-class Goat(Animal):
+class Cow(Milk_Givers):
+    kind = 'Корова'
+    noise = 'Му-у-у'
+
+class Goat(Milk_Givers):
     kind = 'Коза'
-    product = 'Молоко'
     noise = 'Ме-е-е'
 
+class Wool_Givers(Animal):
+    def harvest_wool(self):
+        print('Шерсть собрана')
+
+class Sheep(Wool_Givers):
+    kind = 'Овца'
+    noise = 'Бе-е-е'
 
 seryy = Goose()
 seryy.name = 'Серый'
 seryy.weight = 3
-seryy.feed(0.1)
-seryy.make_noise()
-seryy.harvest(200, 'гр.')
-
-print('')
+seryy.harvest_eggs()
 
 belyy = Goose()
 belyy.name = 'Белый'
 belyy.weight = 2.5
-belyy.feed(0.2)
-belyy.make_noise()
-belyy.harvest(150, 'гр.')
-
-print('')
 
 manka = Cow()
 manka.name = 'Манька'
 manka.weight = 400
-manka.feed(10)
-manka.make_noise()
-manka.harvest(10, 'л.')
-
-print('')
 
 ko_ko = Chiken()
 ko_ko.name = 'Ко-ко'
 ko_ko.weight = 2
-ko_ko.feed(0.1)
-ko_ko.make_noise()
-ko_ko.harvest(1, 'шт.')
-
-print('')
 
 kukareku = Chiken()
 kukareku.name = 'Кукареку'
 kukareku.weight = 2.5
-kukareku.feed(0.1)
-kukareku.noise = 'Ку-ка-ре-ку'
-kukareku.make_noise()
-
-print('')
 
 kriakva = Duck()
 kriakva.name = 'Кряква'
 kriakva.weight = 3
-kriakva.make_noise()
-kriakva.feed(0.1)
-kriakva.harvest(100, 'гр.')
-
-print('')
 
 barashek = Sheep()
 barashek.name = 'Барашек'
 barashek.weight = 120
-barashek.feed(1)
-barashek.make_noise()
-barashek.harvest(6, 'кг.')
-
-print('')
 
 kudriavyy = Sheep()
 kudriavyy.name = 'Кудрявый'
 kudriavyy.weight = 100
-kudriavyy.feed(2)
-kudriavyy.make_noise()
-kudriavyy.harvest(5, 'кг.')
-
-print('')
 
 roga = Goat()
 roga.name = 'Рога'
 roga.weight = 100
-roga.make_noise()
-roga.feed(1)
-roga.harvest(5, 'л.')
-
-print('')
 
 kopyta = Goat()
 kopyta.name = 'Копыта'
 kopyta.weight = 80
-kopyta.make_noise()
-kopyta.feed(1)
-kopyta.harvest(4, 'л.')
-
-print('')
 
 animaldb = AnimalDB()
 animaldb.add_animal(seryy)
@@ -153,15 +107,24 @@ animaldb.add_animal(kudriavyy)
 animaldb.add_animal(roga)
 animaldb.add_animal(kopyta)
 
+def make_noise_together():
+    for animal in AnimalDB.animals_list:
+        animal.make_noise()
+
 def weight_count():
     weight_sum = sum([animal.weight for animal in AnimalDB.animals_list])
     return weight_sum
-
-print(f'Общий вес животных {weight_count():.2f} кг.\n')
 
 def weight_compare():
     weight_max = max([animal.weight for animal in AnimalDB.animals_list])
     animal = [animal.name for animal in AnimalDB.animals_list if animal.weight == weight_max]
     return animal
 
+def feed_everyone():
+    for animal in AnimalDB.animals_list:
+        animal.feed()
+
+feed_everyone()
+make_noise_together()
+print(f'Общий вес животных {weight_count():.2f} кг.\n')
 print(f'Самое тяжелое животное: {weight_compare()[0]}')
